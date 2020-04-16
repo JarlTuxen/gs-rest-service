90ab3b6a09fbaf602ba7ebda2fdc8c1460d68861
JarlTuxen <jarl.tuxen@gmail.com>
2020-04-16 12:29:10 +0200

    opgave RestController

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -2,10 +2,9 @@ package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class GreetingController {
 
@@ -16,4 +15,28 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@PutMapping("/greeting/{id}")
+	public Greeting greetingUpdate(@PathVariable("id") long id, @RequestBody Greeting greeting ) {
+		return new Greeting(id + greeting.getId(), greeting.getContent());
+	}
+
+	/*
+
+	@GetMapping
+	public ResponseEntity<List<Greeting>> listAll()
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Greeting> findGreetingById(@PathVariable("id") long id)
+
+	@PostMapping
+	public ResponseEntity<Greeting> createGreeting(@RequestBody Greeting greeting)
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Greeting> updateGreeting(@PathVariable("id") long id, @RequestBody Greeting greeting)
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteGreeting(@PathVariable("id") long id)
+
+	 */
 }
